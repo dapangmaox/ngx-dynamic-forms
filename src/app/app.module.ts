af313f9c92d57e091e5b70b67917ccd8e237303f
dapangmaox <oliver.xinwei.liang@gmail.com>
2023-05-25 23:32:46 +0800

    feat: update path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from '@ngx-dynamic-forms/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
